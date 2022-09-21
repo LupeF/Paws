40cd8d76e24cb56b3c8a94056057ef57826aea9b
LupeF <46634202+LupeF@users.noreply.github.com>
2022-09-21 10:42:58 -0700

    add opacity to nav links

--- js/app.js
+++ js/app.js
@@ -1,7 +1,6 @@
 const burgerCont = document.getElementById('burger-container');
 const navBtn = document.getElementById('hamburger-btn');
 const navLinks = document.querySelector('nav ul');
-
 burgerCont.addEventListener('click', () =>{
     navLinks.classList.toggle('open');
 })
